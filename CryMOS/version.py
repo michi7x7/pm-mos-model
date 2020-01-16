b0b15a234317dc966b357cdb0bb54bbf0de6de28
Michael Sieberer <michael.sieberer@infineon.com>
2020-01-16 12:50:03 +0100

    update version

--- CryMOS/version.py
+++ CryMOS/version.py
@@ -2,18 +2,30 @@ __version__ = None
 
 try:  # try to get version from git
     from subprocess import run
+    import re
     from os.path import dirname
-    dir = dirname(__file__)
+    wdir = dirname(__file__)
 
-    proc = run('git tag -l --sort=-v:refname',
-               capture_output=True, shell=True, check=True, text=True, cwd=dir)
-    tags = [t for t in proc.stdout.split() if t.startswith('v')]
-    ver = tags[0].replace('v', '')
+    proc = run('git describe --dirty --tags',
+               capture_output=True, shell=True, text=True, cwd=wdir)  # check=True,
+    tagname = proc.stdout.strip()
 
-    proc = run('git log --abbrev-commit --format=%h -1',
-               capture_output=True, shell=True, check=True, text=True, cwd=dir)
-    hash = proc.stdout.strip()
+    if proc.returncode != 0 or not tagname:
+        proc = run('git show --format=%h -s',
+                   capture_output=True, shell=True, check=True, text=True, cwd=wdir)
+        h = proc.stdout.strip()
+        ver = f'0.dev+git.{h}'
+    elif re.fullmatch(r'v[0-9]+(\.[0-9]+){0,2}', tagname):
+        ver = tagname[1:]
+    else:
+        m = re.fullmatch(
+            r'v(?P<version>[0-9]+(\.[0-9]+){0,2})-(?P<dist>\d+)-g(?P<hash>[0-9a-f]+)(?P<dirty>-dirty)?',
+            tagname)
+        assert m is not None, "git describe not parsed"
+        ver = f'{m.group("version")}.dev{m.group("dist")}+git.{m.group("hash")}'
+        if m.group('dirty'):
+            ver += '.dirty'
 
-    __version__ = ver + '.dev0+git.' + hash
+    __version__ = ver
 except:
     pass
